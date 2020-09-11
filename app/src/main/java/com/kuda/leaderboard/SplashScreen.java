6aeb6b4a643187f607c76cfb30a18f61e8b79a98
kudachabs <kudachabs@gmail.com>
2020-09-11 21:20:38 +0200

    Initial commit

---
+++ app/src/main/java/com/kuda/leaderboard/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.kuda.leaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
